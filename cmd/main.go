014a1514cc3033b84a72b2645b3f8e437b63113e
Ruslan <noname0263@gmail.com>
2025-04-30 20:11:37 +0900

    добавил клиента и хендлер в мейн

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"github.com/cockroachdb/errors"
 	"log"
-	"main/internal/app"
+	anti_spoof_client "main/internal/anti-spoof-client"
 	"main/internal/bot"
 	"main/internal/pkg/config"
 	"main/internal/pkg/logger"
+	update_handler "main/internal/update-handler"
 	"os"
 	"time"
 )
@@ -33,20 +34,22 @@ func bootstrap(ctx context.Context) error {
 		return errors.Wrap(err, "config.Load")
 	}
 
-	// 3. Инициализация логгера
 	zLog, err := logger.New(conf.Env)
 	if err != nil {
 		return errors.Wrap(err, "logger.New")
 	}
 	zLog.Infow("Logger and config initialized successfully")
 
-	b, err := bot.New(conf.Bot, conf.Env, &zLog)
+	botExecutor, err := bot.New(conf.Bot, conf.Env, &zLog)
 	if err != nil {
 		return errors.Wrap(err, "New")
 	}
 
-	a := app.New(b, zLog)
-	if err = a.Run(ctx); err != nil {
+	client := anti_spoof_client.NewClient(conf.App)
+
+	updateHandler := update_handler.New(botExecutor, client, zLog)
+
+	if err = updateHandler.Run(ctx); err != nil {
 		return err
 	}
 
