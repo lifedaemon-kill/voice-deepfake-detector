0402fc3b9a66b86853746f1c799a36bb18a84fbc
Ruslan <noname0263@gmail.com>
2025-04-30 20:10:51 +0900

    добавил модель для рест запроса

--- internal/pkg/models/models.go
+++ internal/pkg/models/models.go
@@ -1,1 +1,18 @@
 package models
+
+import "fmt"
+
+type AntiSpoofingResponse struct {
+	MelodyMachine float64 `json:"MelodyMachine"`
+	DavidCombei   float64 `json:"DavidCombei"`
+}
+
+func (p *AntiSpoofingResponse) GetAnswer() bool {
+	return ((p.MelodyMachine + p.DavidCombei) / 2) > 0.7
+}
+
+func (p *AntiSpoofingResponse) ToString() string {
+	return fmt.Sprintf(
+		"Вероятность спуфинга:\nмодель MelodyMachine: %s\nмодель DavidCombei: %s\nЗаключение: %v",
+		p.MelodyMachine, p.MelodyMachine, p.DavidCombei)
+}
