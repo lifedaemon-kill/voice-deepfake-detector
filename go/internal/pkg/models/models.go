ddabcd5d7b1950886c020c036f5108bae02c7465
Ruslan <noname0263@gmail.com>
2025-05-01 18:30:13 +0900

    добавил подсчет взвешенного среднего

--- go/internal/pkg/models/models.go
+++ go/internal/pkg/models/models.go
@@ -7,13 +7,14 @@ type AntiSpoofingResponse struct {
 	MoTheCreator  float64 `json:"mo-thecreator"`
 }
 
-func (p *AntiSpoofingResponse) GetWeightedResult() float64 {
+func (p *AntiSpoofingResponse) WeightedAverage() float64 {
+	//Взвешенное среднее нужно для того, чтобы менее точная модель имела меньшее влияние на результат
 	return (p.MelodyMachine*1.0 + p.MoTheCreator*0.6) / (1.0 + 0.6)
 }
 
-func (p *AntiSpoofingResponse) GetAnswer() string {
+func (p *AntiSpoofingResponse) Predict() string {
 	threshold := 0.7
-	if p.GetWeightedResult() > threshold {
+	if p.WeightedAverage() > threshold {
 		return "Аудио подделано"
 	} else {
 		return "Аудио настоящее"
@@ -23,5 +24,5 @@ func (p *AntiSpoofingResponse) GetAnswer() string {
 func (p *AntiSpoofingResponse) ToString() string {
 	return fmt.Sprintf(
 		"Вероятность спуфинга:\n\tмодель MelodyMachine: %f\n\tмодель mo-thecreator: %f\nСреднее значение: %f\nВзвешенное значение: %f\n\nЗаключение: %v",
-		p.MelodyMachine, p.MoTheCreator, (p.MelodyMachine+p.MoTheCreator)/2, p.GetWeightedResult(), p.GetAnswer())
+		p.MelodyMachine, p.MoTheCreator, (p.MelodyMachine+p.MoTheCreator)/2, p.WeightedAverage(), p.Predict())
 }
